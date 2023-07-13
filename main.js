d4ad98d8ba50d32eef0bc2b25eb4b3a91450e948
w1y0n0 <wiyono30@gmail.com>
2023-07-13 13:32:57 +0700

    tipe data

--- main.js
+++ main.js
@@ -14,5 +14,17 @@ alert("lalala")
 // usia = 40
 // console.log('heyhooo usia kamu adalah ' + usia)
 
+// let usia = prompt('berapa usia kamu ?')
+// alert('usia anda adalah ' + usia)
 
+let nama = 'labsi project' // tipe string
+let usia = 30 // tipe integer number
+let tinggiBadan = 173.5 //tipe double float
+let beratBadan
+let pacar = null
+
+beratBadan = 60
+
+// alert('nama anda adalah ' + nama + ' dan usia anda adalah ' + usia)
+alert(`nama saya ${nama} usia saya itu ${usia} tinggi badan saya adalah ${tinggiBadan} berat badan saya ${beratBadan} dan pacar saya ${pacar}`)
 
